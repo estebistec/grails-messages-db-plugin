a0066df352e355bbf684228e5b5d3f9b5807dacc
Steven Cummings <estebistec@gmail.com>
2010-05-21 14:58:11 -0500

    Changed example alternate language

--- test/projects/sample/grails-app/conf/BootStrap.groovy
+++ test/projects/sample/grails-app/conf/BootStrap.groovy
@@ -3,7 +3,7 @@ import org.estebistec.grails.plugins.messages.db.Message
 class BootStrap {
      def init = { servletContext ->
          new Message(code:'code', locale:Locale.US, text:'US code Text. Arg {0}').save()
-         new Message(code:'code', locale:new Locale("es", "", ""), text:'ES code Text. Arg {0}').save()
+         new Message(code:'code', locale:Locale.FRENCH, text:'FR code Text. Arg {0}').save()
      }
      def destroy = {
      }
